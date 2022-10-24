708bde8dde8ec7a5dc15e3418bf97ab828dc936b
kaiolabs <peypro1@gmail.com>
2022-10-23 22:51:57 -0300

    Criando o favorited_button

---
+++ lib/src/core/presenters/shared/favorited_button.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:pokedex/src/core/presenters/theme/color_outlet.dart';
+
+class FavoritedButton extends StatefulWidget {
+  ValueNotifier<bool> isFavorited;
+  final double size;
+  FavoritedButton({super.key, required this.isFavorited, required this.size});
+
+  @override
+  State<FavoritedButton> createState() => _FavoritedButtonState();
+}
+
+class _FavoritedButtonState extends State<FavoritedButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: widget.isFavorited,
+      builder: (context, value, child) => InkWell(
+        overlayColor: MaterialStateProperty.all(Colors.transparent),
+        child: Icon(
+          (widget.isFavorited.value) ? Icons.favorite : Icons.favorite_border_sharp,
+          color: ColorOutlet.colorWhite,
+          size: widget.size,
+        ),
+        onTap: () {
+          widget.isFavorited.value = !widget.isFavorited.value;
+        },
+      ),
+    );
+  }
+}
