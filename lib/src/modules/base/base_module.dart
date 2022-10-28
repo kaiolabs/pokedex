f6f621839a74d58b3c8ad97d0cede8658f633f5d
kaiolabs <peypro1@gmail.com>
2022-10-27 22:10:34 -0300

    Adicionado novas propriedades ao base_module

--- lib/src/modules/base/base_module.dart
+++ lib/src/modules/base/base_module.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pokedex/main.dart';
 import 'package:pokedex/src/modules/base/controllers/base_controller.dart';
 import 'package:pokedex/src/modules/base/controllers/favorites_controller.dart';
 import 'package:pokedex/src/modules/base/controllers/home_controller.dart';
@@ -10,12 +12,18 @@ class BaseModule extends Module {
   List<Bind> get binds => [
         Bind.factory((i) => HomeController()),
         Bind.factory((i) => PokedexController()),
-        Bind.factory((i) => FavoritesController()),
-        Bind.factory((i) => BaseController(
-              favoritesController: i.get(),
-              homeController: i.get(),
-              pokedexController: i.get(),
-            )),
+        Bind.factory((i) => FavoritesController(objectBoxDB: i.get())),
+        Bind.lazySingleton((i) => objectbox),
+        Bind.factory(
+          (i) => BaseController(
+            favoritesController: i.get(),
+            homeController: i.get(),
+            pokedexController: i.get(),
+            pageController: PageController(initialPage: 0),
+            basePageIndex: ValueNotifier<int>(0),
+            objectBoxDB: i.get(),
+          ),
+        ),
       ];
 
   @override
