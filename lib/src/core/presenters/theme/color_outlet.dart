5ce324e699ec314dd0c9e6832ad50e1e0fa1ab7d
kaiolabs <peypro1@gmail.com>
2022-10-27 21:34:16 -0300

    Criando o color_outlet

--- lib/src/core/presenters/theme/color_outlet.dart
+++ lib/src/core/presenters/theme/color_outlet.dart
@@ -6,8 +6,10 @@ abstract class ColorOutlet {
   static const Color colorWhite = Color(0xFFFFFFFF);
   static const Color colorShadow = Color(0x824B4949);
   static const Color colorGrey = Color(0xFF86869E);
+  static const Color colorGreyLight = Color(0xFFE5E5E5);
   static const Color colorTransparent = Color(0x00000000);
   static const Color colorBlack = Color(0xFF000000);
+  static const Color colorBlackLight = Color(0xFF373737);
 
   //card colors
   static const Color colorCardBlack = Color(0xFF1E1E1E);
@@ -15,9 +17,12 @@ abstract class ColorOutlet {
   static const Color colorCardBrown = Color(0xFFA37F5F);
   static const Color colorCardGray = Color(0xFF86869E);
   static const Color colorCardGreen = Color(0xFF4E9D5D);
+  static const Color colorCardGreenDark = Color(0xFF2E7D32);
   static const Color colorCardPink = Color(0xFFD95F9F);
   static const Color colorCardPurple = Color(0xFFAD54FB);
   static const Color colorCardRed = Color(0xFFD95F5F);
   static const Color colorCardWhite = Color(0xFFCDCDCD);
   static const Color colorCardYellow = Color(0xFFE8C54F);
+
+  static const Color colorGlass = Color(0x7F000000);
 }
