7e9f06bc2c1f62aac2ab4fd1d21e217d59e1f51b
kaiolabs <peypro1@gmail.com>
2022-10-27 21:35:51 -0300

    Criando o base_controller

--- lib/src/modules/base/controllers/base_controller.dart
+++ lib/src/modules/base/controllers/base_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:pokedex/src/core/repositories/objectbox_db.dart';
 import 'package:pokedex/src/modules/base/controllers/favorites_controller.dart';
 import 'package:pokedex/src/modules/base/controllers/home_controller.dart';
 import 'package:pokedex/src/modules/base/controllers/pokedex_controller.dart';
@@ -11,17 +12,38 @@ class BaseController extends ChangeNotifier {
   final HomeController homeController;
   final PokedexController pokedexController;
   final FavoritesController favoritesController;
+  PageController pageController = PageController(initialPage: 0);
+  ValueNotifier<int> basePageIndex = ValueNotifier<int>(0);
+  final ObjectBoxDB objectBoxDB;
 
   BaseController({
     required this.homeController,
     required this.pokedexController,
     required this.favoritesController,
+    required this.pageController,
+    required this.basePageIndex,
+    required this.objectBoxDB,
   }) : pages = [
-          HomeView(controller: homeController),
-          PokedexView(controller: pokedexController),
-          FavoritesView(controller: favoritesController),
+          HomeView(
+            controller: homeController,
+            pageController: pageController,
+            basePageIndex: basePageIndex,
+            pokedexController: pokedexController,
+            objectBoxDB: objectBoxDB,
+            favoritesController: favoritesController,
+          ),
+          PokedexView(
+            controller: pokedexController,
+            pageController: pageController,
+            objectBoxDB: objectBoxDB,
+            basePageIndex: basePageIndex,
+          ),
+          FavoritesView(
+            controller: favoritesController,
+            objectBoxDB: objectBoxDB,
+            basePageIndex: basePageIndex,
+            pageController: pageController,
+            pokedexController: pokedexController,
+          ),
         ];
-
-  PageController pageController = PageController(initialPage: 0);
-  ValueNotifier<int> basePageIndex = ValueNotifier<int>(0);
 }
